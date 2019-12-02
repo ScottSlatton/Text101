ddaa3835aa44f6fe35636528b3ea43e21f32f9b0
Scott Slatton <scottslatton.tech@gmail.com>
2019-12-01 17:07:51 -0800

    Created ability for player to choose between options.

--- Assets/State.cs
+++ Assets/State.cs
@@ -7,11 +7,19 @@ using UnityEngine;
 public class State : ScriptableObject
 {
 
-   [TextArea(14,10)][SerializeField] string storyText;
+    [TextArea(14,10)][SerializeField] string storyText;
+    [SerializeField] State[] nextStates;
+
+
+
 
     public string GetStateStory()
     {
         return storyText;
     }
 
+    public State[] GetNextStates()
+    {
+        return nextStates;
+    }
 }
